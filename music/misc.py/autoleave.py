263291bbedca9a6473ce976f9b547b507f944c80
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-05 02:30:41 +0300

    Update autoleave.py

--- music/misc.py/autoleave.py
+++ music/misc.py/autoleave.py
@@ -13,7 +13,7 @@ async def auto_leave():
         while not await asyncio.sleep(
             config.AUTO_LEAVE_ASSISTANT_TIME
         ):
-            from YukkiMusic.core.userbot import assistants
+            from AnonX.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
@@ -57,7 +57,7 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Yukki.stop_stream(chat_id)
+                    await Anon.stop_stream(chat_id)
                 except:
                     continue
                 try:

208aa303a2bd64fa2b331640cb42645be3df596a
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-05 04:08:27 +0300

    Update mute.py

--- music/admins.py/mute.py
+++ music/admins.py/mute.py
@@ -5,7 +5,7 @@ from config import BANNED_USERS
 from strings import get_command
 from strings.filters import command
 from AnonX import app
-from AnonX.core.call import Yukki
+from AnonX.core.call import AnonX
 from AnonX.utils.database import is_muted, mute_on
 from AnonX.utils.decorators import AdminRightsCheck
 

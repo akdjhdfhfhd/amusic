94a1bfefc0232f04b7acdb6180ebc448471e72e8
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-06 04:12:29 +0300

    Update ping.py

--- music/tools/ping.py
+++ music/tools/ping.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from datetime import datetime
 
 from pyrogram import filters
@@ -15,10 +6,10 @@ from pyrogram.types import Message
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from strings.filters import command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import bot_sys_stats
-from YukkiMusic.utils.decorators.language import language, languageCB
+from AnonX import app
+from AnonX.core.call import Anon 
+from AnonX.utils import bot_sys_stats
+from AnonX.utils.decorators.language import language, languageCB
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")

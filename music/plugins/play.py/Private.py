6f45ee1e4df0dad6283074433add70d14646c30c
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-07 05:11:10 +0300

    Update Private.py

--- music/plugins/play.py/Private.py
+++ music/plugins/play.py/Private.py
@@ -6,7 +6,7 @@ from strings import get_command
 from strings.filters import command
 
 from pyrogram.types import Message , ReplyKeyboardMarkup , KeyboardButton
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 #############################
 
